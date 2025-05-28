58ca219af982915660fbf0452647c1b3b6051ba8
fahrezapratamahidayat <fahrezaxgamer@gmail.com>
2025-05-28 22:59:24 +0700

    feat: integrate theme provider and screen utility for dynamic theming in main app

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
 import 'package:flashfeed/src/config/app_routes.dart';
+import 'package:flashfeed/src/config/app_theme.dart';
+import 'package:flashfeed/src/config/theme_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(MyApp());
+  await SharedPreferences.getInstance();
+  runApp(
+    ChangeNotifierProvider(create: (_) => ThemeProvider(), child: MyApp()),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -13,12 +21,25 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'News App',
-      debugShowCheckedModeBanner: false,
-      // theme: AppTheme.light,
-      initialRoute: AppRoutes.splash,
-      onGenerateRoute: AppRoutes.generateRoute,
+    return Consumer<ThemeProvider>(
+      builder: (context, themeProvider, _) {
+        return ScreenUtilInit(
+          designSize: const Size(360, 960),
+          minTextAdapt: true,
+          splitScreenMode: true,
+          builder: (_, child) {
+            return MaterialApp(
+              title: 'News App',
+              debugShowCheckedModeBanner: false,
+              theme: AppTheme.light,
+              darkTheme: AppTheme.dark,
+              themeMode: themeProvider.themeMode,
+              initialRoute: AppRoutes.splash,
+              onGenerateRoute: AppRoutes.generateRoute,
+            );
+          },
+        );
+      },
     );
   }
 }
