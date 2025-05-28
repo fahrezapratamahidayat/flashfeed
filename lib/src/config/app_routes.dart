f0bafb60af50cbc4da7168f1edf2b6099b392345
fahrezapratamahidayat <fahrezaxgamer@gmail.com>
2025-05-28 22:59:31 +0700

    feat: add routes for forgot password and home screens in app_routes.dart

--- lib/src/config/app_routes.dart
+++ lib/src/config/app_routes.dart
@@ -1,5 +1,8 @@
+import 'package:flashfeed/src/presentation/pages/forgot_password_screen.dart';
+import 'package:flashfeed/src/presentation/pages/home_screen.dart';
 import 'package:flashfeed/src/presentation/pages/introduction_screen.dart';
 import 'package:flashfeed/src/presentation/pages/login_screen.dart';
+import 'package:flashfeed/src/presentation/pages/not_found_screen.dart';
 import 'package:flashfeed/src/presentation/pages/register_screen.dart';
 import 'package:flashfeed/src/presentation/pages/splash_screen.dart';
 import 'package:flutter/cupertino.dart';
@@ -10,6 +13,7 @@ class AppRoutes {
   static const introduction = '/intro';
   static const login = '/login';
   static const register = '/register';
+  static const forgotPassword = 'forgotPassword';
   static const home = '/home';
   static const articleDetail = '/article/:id';
 
@@ -23,10 +27,13 @@ class AppRoutes {
         return MaterialPageRoute(builder: (_) => LoginScreen());
       case register:
         return MaterialPageRoute(builder: (_) => RegisterScreen());
+      case forgotPassword:
+        return MaterialPageRoute(builder: (_) => ForgotPasswordScreen());
+
+      case home:
+        return MaterialPageRoute(builder: (_) => HomeScreen());
       default:
-        return MaterialPageRoute(
-          builder: (_) => Scaffold(body: Center(child: Text('Page not found'))),
-        );
+        return MaterialPageRoute(builder: (_) => NotFoundScreen());
     }
   }
 }
