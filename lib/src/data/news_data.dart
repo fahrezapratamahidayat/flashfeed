b644189dbe029b51423ac19c9bab56a10d4170ce
fahrezapratamahidayat <fahrezaxgamer@gmail.com>
2025-05-29 21:53:33 +0700

    feat: add news categories data structure in news_data.dart for organizing news types

---
+++ lib/src/data/news_data.dart
@@ -0,0 +1,57 @@
+final List<Map<String, String>> newsCategories = [
+  {
+    'label': 'Terkini',
+    'value': 'terkini',
+    'url': 'https://berita-indo-api-next.vercel.app/api/antara-news/terkini',
+  },
+  {
+    'label': 'Top News',
+    'value': 'top-news',
+    'url': 'https://berita-indo-api-next.vercel.app/api/antara-news/top-news',
+  },
+  {
+    'label': 'Politik',
+    'value': 'politik',
+    'url': 'https://berita-indo-api-next.vercel.app/api/antara-news/politik',
+  },
+  {
+    'label': 'Hukum',
+    'value': 'hukum',
+    'url': 'https://berita-indo-api-next.vercel.app/api/antara-news/hukum',
+  },
+  {
+    'label': 'Ekonomi',
+    'value': 'ekonomi',
+    'url': 'https://berita-indo-api-next.vercel.app/api/antara-news/ekonomi',
+  },
+  {
+    'label': 'Metro',
+    'value': 'metro',
+    'url': 'https://berita-indo-api-next.vercel.app/api/antara-news/metro',
+  },
+  {
+    'label': 'Sepak Bola',
+    'value': 'sepakbola',
+    'url': 'https://berita-indo-api-next.vercel.app/api/antara-news/sepakbola',
+  },
+  {
+    'label': 'Olahraga',
+    'value': 'olahraga',
+    'url': 'https://berita-indo-api-next.vercel.app/api/antara-news/olahraga',
+  },
+  {
+    'label': 'Hiburan',
+    'value': 'hiburan',
+    'url': 'https://berita-indo-api-next.vercel.app/api/antara-news/hiburan',
+  },
+  {
+    'label': 'Teknologi',
+    'value': 'teknologi',
+    'url': 'https://berita-indo-api-next.vercel.app/api/antara-news/teknologi',
+  },
+  {
+    'label': 'Gaya Hidup',
+    'value': 'lifestyle',
+    'url': 'https://berita-indo-api-next.vercel.app/api/antara-news/lifestyle',
+  },
+];
