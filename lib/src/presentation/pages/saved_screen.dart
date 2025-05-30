7bf2e5afec78f41d98edf0c8acfcc0e89df79552
fahrezapratamahidayat <fahrezaxgamer@gmail.com>
2025-05-30 18:37:16 +0700

    feat: create SavedScreen widget to display saved items, enhancing user navigation and content management within the app

---
+++ lib/src/presentation/pages/saved_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class SavedScreen extends StatefulWidget {
+  const SavedScreen({super.key});
+
+  @override
+  State<SavedScreen> createState() => _SavedScreenState();
+}
+
+class _SavedScreenState extends State<SavedScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: SafeArea(child: Column(children: [Text('data')])),
+    );
+  }
+}
