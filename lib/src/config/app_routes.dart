c7da3be3fc207348704cf62efeb5f826362dc763
fahrezapratamahidayat <fahrezaxgamer@gmail.com>
2025-05-30 18:36:22 +0700

    feat: add new routes for ProfileScreen, ExploreScreen, TrendingScreen, and SavedScreen to enhance navigation and user experience

--- lib/src/config/app_routes.dart
+++ lib/src/config/app_routes.dart
@@ -1,11 +1,15 @@
 import 'package:flashfeed/src/presentation/pages/articel_detail_screen.dart';
+import 'package:flashfeed/src/presentation/pages/explore_screen.dart';
 import 'package:flashfeed/src/presentation/pages/forgot_password_screen.dart';
 import 'package:flashfeed/src/presentation/pages/home_screen.dart';
 import 'package:flashfeed/src/presentation/pages/introduction_screen.dart';
 import 'package:flashfeed/src/presentation/pages/login_screen.dart';
 import 'package:flashfeed/src/presentation/pages/not_found_screen.dart';
+import 'package:flashfeed/src/presentation/pages/profile_screen.dart';
 import 'package:flashfeed/src/presentation/pages/register_screen.dart';
+import 'package:flashfeed/src/presentation/pages/saved_screen.dart';
 import 'package:flashfeed/src/presentation/pages/splash_screen.dart';
+import 'package:flashfeed/src/presentation/pages/trending_screen.dart';
 import 'package:flutter/material.dart';
 
 class AppRoutes {
@@ -16,6 +20,10 @@ class AppRoutes {
   static const forgotPassword = 'forgotPassword';
   static const home = '/home';
   static const articleDetail = '/article/:id';
+  static const profile = '/profile';
+  static const explore = '/explore';
+  static const trending = '/trending';
+  static const saved = '/saved';
 
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
@@ -36,6 +44,15 @@ class AppRoutes {
         return MaterialPageRoute(
           builder: (_) => ArticleDetailPage(articleId: articleId),
         );
+      case profile:
+        return MaterialPageRoute(builder: (_) => ProfileScreen());
+      case explore:
+        return MaterialPageRoute(builder: (_) => ExploreScreen());
+      case trending:
+        return MaterialPageRoute(builder: (_) => TrendingScreen());
+      case saved:
+        return MaterialPageRoute(builder: (_) => SavedScreen());
+
       default:
         return MaterialPageRoute(builder: (_) => NotFoundScreen());
     }
